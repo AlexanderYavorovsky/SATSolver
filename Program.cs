e591e4380c9f74e76fc429a39c36f10062b4fd4f
AlexanderYavorovsky <aleyavoff@gmail.com>
2024-04-06 20:27:55 +0300

    feat: add project

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace SATSolver;
+
+class Program()
+{
+    static void Main(string[] args)
+    {
+        string path = args[0];
+
+        SAT sat = new SAT();
+        Parser parser = new Parser();
+        sat.CNF = parser.Parse(path);
+        
+        (bool status, var solution) = sat.DPLL(new List<int>());
+        if (!status)
+        {
+            Console.WriteLine("s UNSATISFIABLE");
+            return;
+        }
+        Console.WriteLine("s SATISFIABLE");
+        Console.Write("v ");
+        foreach (var x in solution)   
+        {
+            Console.Write(x + " ");
+        }
+        Console.WriteLine("0");
+    }
+}
