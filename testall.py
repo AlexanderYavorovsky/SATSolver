3731f2caff0ce0a25082e35e71ede171c5e13df5
AlexanderYavorovsky <aleyavoff@gmail.com>
2024-04-06 20:24:23 +0300

    test: add testers

---
+++ testall.py
@@ -0,0 +1,22 @@
+import subprocess
+import os
+import sys
+
+tests_num = 4
+test_dir = 'tests'
+returncode = 0
+
+subprocess.run(["echo", "Starting tests..."])
+
+for file in os.listdir(test_dir):
+    if file.endswith('.txt'):
+        status = subprocess.run(
+            ["python3", "test.py", os.path.join(test_dir, file)],
+            capture_output=True,
+            text=True
+            )
+        print(status.stdout.strip('\n'))
+        if (status.returncode != 0):
+            returncode = 1 
+
+sys.exit(returncode)
