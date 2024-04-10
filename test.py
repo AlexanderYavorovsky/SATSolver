07603f94ca70c18f21d83b3b51ab5ff65de760a6
AlexanderYavorovsky <aleyavoff@gmail.com>
2024-04-10 16:56:24 +0300

    test: modify tester

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import subprocess
 import tempfile
 import sys
+from colorama import Fore
 
 sat_msg = 's SATISFIABLE'
 unsat_msg = 's UNSATISFIABLE'
@@ -12,9 +13,6 @@ result = subprocess.run(
     universal_newlines=True
     )
 
-# print('My out:')
-# print(result.stdout)
-
 tmp = tempfile.NamedTemporaryFile()
 with open(tmp.name, 'w') as f:
     f.write(result.stdout)
@@ -22,7 +20,6 @@ with open(tmp.name, 'w') as f:
 
 with open(tmp.name, 'r') as f:
     status = f.readline().strip('\n')
-    # print(status)
 
     if (status == unsat_msg):
         picosat_status = subprocess.run(
@@ -30,32 +27,27 @@ with open(tmp.name, 'r') as f:
             capture_output=True,
             universal_newlines=True
             )
-        # print(picosat_status.stdout.strip('\n'))
         if (picosat_status.stdout.strip('\n') == unsat_msg):
-            print(f'{in_file_name}: PASSED')
+            print(f'{in_file_name}: {Fore.GREEN}PASSED{Fore.LIGHTWHITE_EX}')
             sys.exit(0)
         else:
-            print(f'{in_file_name}: FAILED')
+            print(f'{in_file_name}: {Fore.RED}FAILED{Fore.LIGHTWHITE_EX}')
             sys.exit(1)
     else:
         solution = f.readline().strip('\n')[2:]
         tmp_in_file = tempfile.NamedTemporaryFile()
-        # shutil.copyfile(in_file_name, tmp_in_file.name)
 
         # copy from in_file_name to tmp file,
         # change header and add solution literals as new clauses
         with open(tmp_in_file.name, 'w') as f_in_tmp:
             with open(in_file_name, 'r') as f_in:
                 for line in f_in:
-                    # print(line.strip('\n'))
                     if line[0] == 'p':
                         splitted = line.split()
                         new_line = ' '.join(splitted[:3]) + ' ' + \
                             str(int(splitted[2]) + int(splitted[3]))
-                        # print('Myline: ', new_line)
                     else:
                         new_line = line.strip('\n')
-                    # print('Writing:', new_line, 'end')
                     f_in_tmp.write(new_line + '\n')
 
             # convert solution to clauses
@@ -63,19 +55,16 @@ with open(tmp.name, 'r') as f:
                 if literal == '0':
                     break
                 f_in_tmp.write(literal + ' 0\n')
-                # print('Wrote literal: ', literal)
 
-        # print('runnning:', f_in_tmp.name)
         picosat_status = subprocess.run(
             ['picosat', f_in_tmp.name],
             capture_output=True,
             universal_newlines=True
             )
 
-        # print(picosat_status.stdout.split('\n')[0])
         if (picosat_status.stdout.split('\n')[0] == sat_msg):
-            print(f'{in_file_name}: PASSED')
+            print(f'{in_file_name}: {Fore.GREEN}PASSED{Fore.LIGHTWHITE_EX}')
             sys.exit(0)
         else:
-            print(f'{in_file_name}: FAILED')
+            print(f'{in_file_name}: {Fore.RED}FAILED{Fore.LIGHTWHITE_EX}')
             sys.exit(1)
